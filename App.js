23647753bb8307a47b94a0ea45af273df1863481
Alex Doss <alex.doss42@gmail.com>
2019-08-26 14:29:40 -0600

    Adds state hooks and maps list of goals to display in list below

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [ enteredGoal, setEnteredGoal ] = useState('')
+  const [ courseGoals, setCourseGoals ] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals([...courseGoals, enteredGoal])
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
           placeholder='Add goal here' 
           style={styles.input}
+          onChangeText = {goalInputHandler}
+          value = {enteredGoal}
         />
-        <Button title='ADD'/>
+        <Button 
+          title='ADD'
+          onPress={addGoalHandler}
+        />
       </View>
       <View>
-
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
   );
